bd9af57443f2cd739e25a88c0965a14c128ecbb4
kishoreAravanan <kishoreAravanan@DESKTOP-B1ECJD6>
2020-04-15 18:38:54 +0530

    added the change

--- demo/src/demo/Kishore.java
+++ demo/src/demo/Kishore.java
@@ -19,8 +19,8 @@ public class Kishore extends KK {
 		return s+str;
 	}
 	
-	public static String getName(String id) {
-		return id;
+	public static String getName(String ss) {
+		return ss;
 	}
 	
 
